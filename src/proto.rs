61d5be9cb5c161bbe2e3985ca2dfae0ffee4f22c
Piotr Sarna <sarna@chiselstrike.com>
2023-04-04 09:08:28 +0200

    workers: migrate to WebSocket
    The workers backend is now migrated to use WebSockets
    instead of HTTP. HTTP is temporarily removed, but we can
    bring the support back for performance reasons, if somebody
    is only interested in simple queries and does not care about
    interactive transactions.

--- src/proto.rs
+++ src/proto.rs
@@ -1,6 +1,12 @@
 //! `proto` contains libSQL/sqld/hrana wire protocol.
 
 #[cfg(feature = "hrana_backend")]
-pub use hrana_client::proto::{BatchResult, Col, Error, StmtResult, Value};
+pub use hrana_client::proto::{
+    Batch, BatchReq, BatchResp, BatchResult, ClientMsg, Col, Error, ExecuteReq, ExecuteResp,
+    OpenStreamReq, Request, Response, ServerMsg, Stmt, StmtResult, Value,
+};
 #[cfg(not(feature = "hrana_backend"))]
-pub use hrana_client_proto::{BatchResult, Col, Error, StmtResult, Value};
+pub use hrana_client_proto::{
+    Batch, BatchReq, BatchResp, BatchResult, ClientMsg, Col, Error, ExecuteReq, ExecuteResp,
+    OpenStreamReq, Request, Response, ServerMsg, Stmt, StmtResult, Value,
+};
--- src/workers.rs
+++ src/workers.rs
@@ -1,17 +1,17 @@
 use crate::client::Config;
+use crate::proto;
+use anyhow::Context;
 use async_trait::async_trait;
-use base64::Engine;
 use worker::*;
 
-use crate::{BatchResult, Statement, Transaction};
+use crate::{BatchResult, ResultSet, Statement};
 
 /// Database client. This is the main structure used to
 /// communicate with the database.
-#[derive(Clone, Debug)]
+#[derive(Debug)]
 pub struct Client {
-    base_url: String,
-    url_for_queries: String,
-    auth: String,
+    socket: WebSocket,
+    next_reqid: std::sync::atomic::AtomicI32,
 }
 
 impl Client {
@@ -20,65 +20,77 @@ impl Client {
     /// # Arguments
     /// * `url` - URL of the database endpoint
     /// * `token` - auth token
-    pub fn new(url: impl Into<String>, token: impl Into<String>) -> Self {
+    pub async fn new(url: impl Into<String>, token: impl Into<String>) -> anyhow::Result<Self> {
         let token = token.into();
         let url = url.into();
-        // Auto-update the URL to start with https:// if no protocol was specified
-        let base_url = if !url.contains("://") {
+        // Auto-update the URL to start with https://.
+        // It will get updated to wss via Workers API automatically
+        let url = if !url.contains("://") {
             "https://".to_owned() + &url
+        } else if let Some(url) = url.strip_prefix("libsql://") {
+            "https://".to_owned() + url
+        } else if let Some(url) = url.strip_prefix("wss://") {
+            "https://".to_owned() + url
         } else {
             url
         };
-        let url_for_queries = if cfg!(feature = "separate_url_for_queries") {
-            format!("{base_url}/queries")
-        } else {
-            base_url.clone()
-        };
-        Self {
-            base_url,
-            url_for_queries,
-            auth: format!("Bearer {token}"),
-        }
-    }
+        let url = url::Url::parse(&url).context("Failed to parse URL")?;
 
-    /// Creates a database client with Basic HTTP authentication.
-    ///
-    /// # Arguments
-    /// * `url` - URL of the database endpoint
-    /// * `username` - database username
-    /// * `pass` - user's password
-    pub fn from_credentials(
-        url: impl Into<String>,
-        username: impl Into<String>,
-        pass: impl Into<String>,
-    ) -> Self {
-        let username = username.into();
-        let pass = pass.into();
-        let url = url.into();
-        // Auto-update the URL to start with https:// if no protocol was specified
-        let base_url = if !url.contains("://") {
-            "https://".to_owned() + &url
-        } else {
-            url
+        let mut req =
+            Request::new(url.as_str(), Method::Get).map_err(|e| anyhow::anyhow!("{}", e))?;
+        let headers = req.headers_mut().map_err(|e| anyhow::anyhow!("{}", e))?;
+        headers
+            .set("upgrade", "websocket")
+            .map_err(|e| anyhow::anyhow!("{}", e))?;
+        headers
+            .set("Authentication", &format!("Bearer {token}"))
+            .map_err(|e| anyhow::anyhow!("{}", e))?;
+
+        let res = Fetch::Request(req)
+            .send()
+            .await
+            .map_err(|e| anyhow::anyhow!("{}", e))?;
+
+        let socket = match res.websocket() {
+            Some(ws) => ws,
+            None => anyhow::bail!("server did not accept"),
         };
-        let url_for_queries = if cfg!(feature = "separate_url_for_queries") {
-            format!("{base_url}/queries")
-        } else {
-            base_url.clone()
-        };
-        Self {
-            base_url,
-            url_for_queries,
-            auth: format!(
-                "Basic {}",
-                base64::engine::general_purpose::STANDARD.encode(format!("{username}:{pass}"))
-            ),
-        }
+
+        let mut event_stream = socket.events().map_err(|e| anyhow::anyhow!("{}", e))?;
+
+        socket.accept().map_err(|e| anyhow::anyhow!("{}", e))?;
+
+        let jwt = if token.is_empty() { None } else { Some(token) };
+        socket
+            .send(&proto::ClientMsg::Hello { jwt })
+            .map_err(|e| anyhow::anyhow!("{}", e))?;
+
+        Self::recv_response(&mut event_stream)
+            .await
+            .map_err(|e| anyhow::anyhow!("{}", e))?;
+
+        // NOTICE: only a single stream id is used for now
+        socket
+            .send(&proto::ClientMsg::Request {
+                request_id: 0,
+                request: proto::Request::OpenStream(proto::OpenStreamReq { stream_id: 0 }),
+            })
+            .map_err(|e| anyhow::anyhow!("{}", e))?;
+
+        Self::recv_response(&mut event_stream)
+            .await
+            .map_err(|e| anyhow::anyhow!("{}", e))?;
+        console_log!("Response received");
+        drop(event_stream);
+        Ok(Self {
+            socket,
+            next_reqid: std::sync::atomic::AtomicI32::new(1),
+        })
     }
 
     /// Creates a database client from a `Config` object.
-    pub fn from_config(config: Config) -> Self {
-        Self::new(config.url, config.auth_token.unwrap_or_default())
+    pub async fn from_config(config: Config) -> anyhow::Result<Self> {
+        Self::new(config.url, config.auth_token.unwrap_or_default()).await
     }
 
     /// Creates a database client, given a `Url`
@@ -93,9 +105,9 @@ impl Client {
     /// use url::Url;
     ///
     /// let url  = Url::parse("https://foo:bar@localhost:8080").unwrap();
-    /// let db = Client::from_url(&url).unwrap();
+    /// let db = Client::from_url(url).unwrap();
     /// ```
-    pub fn from_url<T: TryInto<url::Url>>(url: T) -> anyhow::Result<Client>
+    pub async fn from_url<T: TryInto<url::Url>>(url: T) -> anyhow::Result<Client>
     where
         <T as TryInto<url::Url>>::Error: std::fmt::Display,
     {
@@ -103,19 +115,12 @@ impl Client {
             .try_into()
             .map_err(|e| anyhow::anyhow!(format!("{e}")))?;
         let mut params = url.query_pairs();
-        // Try a token=XXX parameter first, continue if not found
-        if let Some((_, token)) = params.find(|(param_key, _)| param_key == "token") {
-            return Ok(Client::new(url.as_str(), token.into_owned()));
+        // Try if a jwt=XXX parameter exists
+        if let Some((_, token)) = params.find(|(param_key, _)| param_key == "jwt") {
+            Client::new(url.as_str(), token).await
+        } else {
+            Client::new(url, "").await
         }
-
-        let username = url.username();
-        let password = url.password().unwrap_or_default();
-        let mut url = url.clone();
-        url.set_username("")
-            .map_err(|_| anyhow::anyhow!("Could not extract username from URL. Invalid URL?"))?;
-        url.set_password(None)
-            .map_err(|_| anyhow::anyhow!("Could not extract password from URL. Invalid URL?"))?;
-        Ok(Client::from_credentials(url.as_str(), username, password))
     }
 
     /// Establishes a database client from Cloudflare Workers context.
@@ -125,82 +130,113 @@ impl Client {
     /// * `LIBSQL_CLIENT_PASS`
     /// # Arguments
     /// * `ctx` - Cloudflare Workers route context
-    pub fn from_ctx<D>(ctx: &worker::RouteContext<D>) -> anyhow::Result<Self> {
-        if let Ok(token) = ctx.secret("LIBSQL_CLIENT_TOKEN") {
-            return Ok(Self::new(
-                ctx.secret("LIBSQL_CLIENT_URL")
-                    .map_err(|e| anyhow::anyhow!("{e}"))?
-                    .to_string(),
-                token.to_string(),
-            ));
-        }
-
-        Ok(Self::from_credentials(
+    pub async fn from_ctx<D>(ctx: &worker::RouteContext<D>) -> anyhow::Result<Self> {
+        let token = ctx
+            .secret("LIBSQL_CLIENT_TOKEN")
+            .map_err(|e| anyhow::anyhow!("{e}"))?;
+        Self::new(
             ctx.secret("LIBSQL_CLIENT_URL")
                 .map_err(|e| anyhow::anyhow!("{e}"))?
                 .to_string(),
-            ctx.secret("LIBSQL_CLIENT_USER")
-                .map_err(|e| anyhow::anyhow!("{e}"))?
-                .to_string(),
-            ctx.secret("LIBSQL_CLIENT_PASS")
-                .map_err(|e| anyhow::anyhow!("{e}"))?
-                .to_string(),
-        ))
+            token.to_string(),
+        )
+        .await
     }
 
-    /// Executes a batch of SQL statements.
-    /// Each statement is going to run in its own transaction,
-    /// unless they're wrapped in BEGIN and END
-    ///
-    /// # Arguments
-    /// * `stmts` - SQL statements
-    ///
-    /// # Examples
-    ///
-    /// ```
-    /// # async fn f() {
-    /// let db = libsql_client::Client::from_credentials("https://example.com", "admin", "s3cr3tp4ss");
-    /// let result = db
-    ///     .batch(["CREATE TABLE t(id)", "INSERT INTO t VALUES (42)"])
-    ///     .await;
-    /// # }
-    /// ```
+    fn send_request(&self, request: proto::Request) -> Result<()> {
+        // NOTICE: we effective allow concurrency of 1 here, until we implement
+        // id allocation andfMe request tracking
+        let request_id = self
+            .next_reqid
+            .fetch_add(1, std::sync::atomic::Ordering::Relaxed);
+        let msg = proto::ClientMsg::Request {
+            request_id,
+            request,
+        };
+
+        self.socket.send(&msg)
+    }
+
+    async fn recv_response(event_stream: &mut EventStream<'_>) -> Result<proto::ServerMsg> {
+        use futures_util::StreamExt;
+
+        // NOTICE: we're effectively synchronously waiting for the response here
+        if let Some(event) = event_stream.next().await {
+            match event? {
+                WebsocketEvent::Message(msg) => {
+                    let stmt_result: proto::ServerMsg = msg.json::<proto::ServerMsg>()?;
+                    Ok(stmt_result)
+                }
+                WebsocketEvent::Close(msg) => {
+                    Err(Error::RustError(format!("connection closed: {msg:?}")))
+                }
+            }
+        } else {
+            Err(Error::RustError("no response".to_string()))
+        }
+    }
+
     async fn raw_batch(
         &self,
         stmts: impl IntoIterator<Item = impl Into<Statement>>,
     ) -> Result<BatchResult> {
-        let mut headers = Headers::new();
-        headers.append("Authorization", &self.auth).ok();
-        let (body, stmts_count) = crate::client::statements_to_string(stmts);
-        let request_init = RequestInit {
-            body: Some(wasm_bindgen::JsValue::from_str(&body)),
-            headers,
-            cf: CfProperties::new(),
-            method: Method::Post,
-            redirect: RequestRedirect::Follow,
-        };
-        let req = Request::new_with_init(&self.url_for_queries, &request_init)?;
-        let response = Fetch::Request(req).send().await;
-        let mut response = match response {
-            Ok(r) if r.status_code() == 200 => r,
-            // Retry with the legacy route: "/"
-            resp => {
-                if cfg!(feature = "separate_url_for_queries") {
-                    Fetch::Request(Request::new_with_init(&self.base_url, &request_init)?)
-                        .send()
-                        .await?
-                } else {
-                    return Err(worker::Error::from(format!("{}", resp?.status_code())));
-                }
+        let mut batch = proto::Batch::new();
+
+        for stmt in stmts.into_iter() {
+            let stmt: Statement = stmt.into();
+            let mut hrana_stmt = proto::Stmt::new(stmt.q, true);
+            for param in stmt.params {
+                hrana_stmt.bind(param);
             }
-        };
-        if response.status_code() != 200 {
-            return Err(worker::Error::from(format!("{}", response.status_code())));
+            batch.step(None, hrana_stmt);
         }
-        let resp: String = response.text().await?;
-        let response_json: serde_json::Value = serde_json::from_str(&resp)?;
-        crate::client::http_json_to_batch_result(response_json, stmts_count)
-            .map_err(|e| worker::Error::from(format!("Error: {} ({:?})", e, request_init.body)))
+
+        let mut event_stream = self.socket.events()?;
+
+        // NOTICE: if we want to support concurrent requests, we need to
+        // actually start managing stream ids
+        self.send_request(proto::Request::Batch(proto::BatchReq {
+            stream_id: 0,
+            batch,
+        }))?;
+
+        match Self::recv_response(&mut event_stream).await? {
+            proto::ServerMsg::ResponseOk {
+                request_id: _,
+                response: proto::Response::Batch(proto::BatchResp { result }),
+            } => Ok(result),
+            proto::ServerMsg::ResponseError {
+                request_id: _,
+                error,
+            } => Err(Error::RustError(format!("{error}"))),
+            _ => Err(Error::RustError("unexpected response".to_string())),
+        }
+    }
+
+    async fn execute(&self, stmt: impl Into<Statement>) -> Result<ResultSet> {
+        let stmt: Statement = stmt.into();
+        let mut hrana_stmt = proto::Stmt::new(stmt.q, true);
+        for param in stmt.params {
+            hrana_stmt.bind(param);
+        }
+
+        let mut event_stream = self.socket.events()?;
+
+        self.send_request(proto::Request::Execute(proto::ExecuteReq {
+            stream_id: 0,
+            stmt: hrana_stmt,
+        }))?;
+        match Self::recv_response(&mut event_stream).await? {
+            proto::ServerMsg::ResponseOk {
+                request_id: _,
+                response: proto::Response::Execute(proto::ExecuteResp { result }),
+            } => Ok(result),
+            proto::ServerMsg::ResponseError {
+                request_id: _,
+                error,
+            } => Err(Error::RustError(format!("{error}"))),
+            _ => Err(Error::RustError("unexpected response".to_string())),
+        }
     }
 }
 
@@ -215,7 +251,7 @@ impl crate::DatabaseClient for Client {
             .map_err(|e| anyhow::anyhow!("{e}"))
     }
 
-    async fn transaction<'a>(&'a self) -> anyhow::Result<Transaction<'a, Self>> {
-        anyhow::bail!("Interactive transactions are only supported by WebSocket (hrana) and local backends. Use batch() instead")
+    async fn execute(&self, stmt: impl Into<Statement>) -> anyhow::Result<ResultSet> {
+        self.execute(stmt).await.map_err(|e| anyhow::anyhow!("{e}"))
     }
 }
