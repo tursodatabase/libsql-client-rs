1c7b4f83f8ff4366f6dd007fdae3ca1bb857c856
Piotr Sarna <sarna@chiselstrike.com>
2023-06-26 21:01:52 +0200

    Add constructors for Config

--- src/client.rs
+++ src/client.rs
@@ -327,3 +327,44 @@ pub struct Config {
     pub url: url::Url,
     pub auth_token: Option<String>,
 }
+
+impl Config {
+    /// Create a new `Config`
+    /// # Examples
+    ///
+    /// ```
+    /// # async fn f() -> anyhow::Result<()> {
+    /// # use libsql_client::Config;
+    /// let config = Config::new("file:////tmp/example.db")?;
+    /// let db = libsql_client::Client::from_config(config).await.unwrap();
+    /// # Ok(())
+    /// # }
+    /// ```
+    pub fn new<T: TryInto<url::Url>>(url: T) -> Result<Self>
+    where
+        <T as TryInto<url::Url>>::Error: std::fmt::Display,
+    {
+        Ok(Self {
+            url: url
+                .try_into()
+                .map_err(|e| anyhow::anyhow!("Failed to parse url: {}", e))?,
+            auth_token: None,
+        })
+    }
+
+    /// Adds an authentication token to config
+    /// # Examples
+    ///
+    /// ```
+    /// # async fn f() -> anyhow::Result<()> {
+    /// # use libsql_client::Config;
+    /// let config = Config::new("https://example.com/db")?.with_auth_token("secret");
+    /// let db = libsql_client::Client::from_config(config).await.unwrap();
+    /// # Ok(())
+    /// # }
+    /// ```
+    pub fn with_auth_token(mut self, token: impl Into<String>) -> Self {
+        self.auth_token = Some(token.into());
+        self
+    }
+}
