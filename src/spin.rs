1f5072eaf75ee0f2af410bdef6853859e25412e3
Piotr Sarna <sarna@turso.tech>
2023-11-10 09:48:25 +0100

    spin: migrate to 2.0
    Some minor API tweaks were needed.

--- src/spin.rs
+++ src/spin.rs
@@ -22,10 +22,8 @@ impl HttpClient {
             .method("POST")
             .body(Some(bytes::Bytes::copy_from_slice(body.as_bytes())))?;
 
-        let response = spin_sdk::outbound_http::send_request(req);
-        let resp: String =
-            std::str::from_utf8(&response?.into_body().unwrap_or_default())?.to_string();
-        let response: pipeline::ServerMsg = serde_json::from_str(&resp)?;
+        let response: http::Response<String> = spin_sdk::http::send(req).await?;
+        let response: pipeline::ServerMsg = serde_json::from_str(&response.into_body())?;
         Ok(response)
     }
 }
